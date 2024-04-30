0ff7dd6122299f8e82d0c4439dd792f9d6598cb6
AiaLans <aiaulimfaizullaeva@gmail.com>
2024-04-30 23:29:47 +0500

    Added JS file

---
+++ script.js
@@ -0,0 +1,44 @@
+function play (userChoice) {
+    var icons = document.querySelectorAll('.icons div');
+
+    var countdown = document.getElementById('count');   
+    countdown.innerHTML = '3';
+
+    var count = 3;
+    var timer = setInterval(function() {
+        count--; 
+        if (count <= 0) {
+            clearInterval(timer);
+            countdown.innerHTML = '';
+            var computerChoice = Math.random();
+            if (computerChoice < 0.34) {
+                computerChoice = 'rock';
+            } else if (computerChoice <= 0.67) {
+                computerChoice = 'paper';
+            } else {
+                computerChoice = 'scissors';
+            }
+            var result = document.getElementById('result');
+            var winner = determineWinner(userChoice, computerChoice);
+            result.innerHTML = winner.text;
+            result.appendChild(winner.image);
+            return;
+        }
+        countdown.innerHTML = count.toString();
+    }, 1000);
+}
+
+function determineWinner(user, computer) {
+    var result = {};
+        
+    if (user === computer) {
+        result.text = "It's a tie!";
+    } else if ((user === 'rock' && computer === 'scissors') ||
+        (user === 'paper' && computer === 'rock') ||
+        (user === 'scissors' && computer === 'paper')) {
+            result.text = "You win!";
+    } else {
+        result.text = "Computer wins!";
+    }
+
+}
